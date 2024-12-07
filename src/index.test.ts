e30b6af38b140861529af0c2181e79ac026c8823
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2024-12-07 13:30:25 +0000

    Correctly format test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,11 @@
 import { isTypeExtended, typeHasKeys } from "@andyrmitchell/utils";
 import type { DdtDialect, DdtDialectDatabaseMap } from ".";
 import type { Dialect } from "drizzle-orm";
+import {test} from 'vitest';
 
+test('Types satisfied', () => {
+    isTypeExtended<DdtDialect, Dialect>(true); // Verify DdtDialects is a subset of drizzle-orms's Dialects
+    typeHasKeys<DdtDialectDatabaseMap, DdtDialect>(true); // Verify coverage of the map includes all DdtDialects    
 
-isTypeExtended<DdtDialect, Dialect>(true); // Verify DdtDialects is a subset of drizzle-orms's Dialects
-typeHasKeys<DdtDialectDatabaseMap, DdtDialect>(true); // Verify coverage of the map includes all DdtDialects
+    expect(true).toBe(true);
+})
